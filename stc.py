43b0b9ce10a5d14b2fe9100fe4fa59f40b135376
HACKE-RC <60568652+HACKE-RC@users.noreply.github.com>
2020-07-08 13:22:43 +0000

    stc.py
    # Now More Faster🤓

--- stc.py
+++ stc.py
@@ -10,47 +10,44 @@ import threading
 import socket
 print('\033[1;33;40m------------[+] Status Code Checker By RC [+]----------\n')
 fname = input('\033[1;37;40mEnter The File Name Which Contains The Subdomains \n\n\033[1;32;40mroot@rc #~ ')
-def full(a):
-	http = urllib3.PoolManager()
-	try:
-		f=open(fname)
-		g=open(fname)
-	except:
-		import os
-		os.system('clear')
-		print("\033[1;34;40m[+] File Not Found [+]")
-		quit
-	#f=open(fname)
-	for i in f:
-		
-		i = i.replace('\n','')
-		try:
-			resp=requests.get(i)
-			if resp.status_code==200:
-				print(f'\033[1;33;40m{i}  200 OK')
-				j=i
-				j = j.replace('https://','')
-				j=j.replace('http://','')
-				cmd=f"nmap -Pn {j}"
-				import os
-				os.system(cmd)
-				#print(j)
-			elif resp.status_code==301:
-				print(f'\033[1;32;40m;;{i} Redirected 301')
-			elif resp.status_code==404:
-				print(f'\033[1;31;40m{i} 404 Not Found')
-			elif resp.status_code==403:
-				print(f"\033[1;36;40m{i} 403 Forbidden/Redirecting")
-			elif resp.status_code==400:
-				print(f'\033[1;30;40m{i} 400 Bad Request')
-			else:
-				print(f"\033[1;34;40m{i} Status : {resp.status_code}")
-		except requests.ConnectionError:
-			print("\033[1;31;40m" + i + " 404 Not Found")
-o = [1]
+http = urllib3.PoolManager()
 try:
-	for i in o:
-		thread = threading.Thread(target=full,args=(1,))
-		thread.start()
+	f=open(fname)
+	g=open(fname)
 except:
-	pass
+	import os
+	os.system('clear')
+	print("\033[1;34;40m[+] File Not Found [+]")
+	quit
+	#f=open(fname)
+
+def full(i):
+	i = i.replace('\n','')
+	try:
+		resp=requests.get(i)
+		if resp.status_code==200:
+			print(f'\033[1;33;40m{i}  200 OK')
+			j=i
+			j = j.replace('https://','')
+			j=j.replace('http://','')
+			cmd=f"nmap -Pn {j}"
+			import os
+			os.system(cmd)
+			#print(j)
+		elif resp.status_code==301:
+			print(f'\033[1;32;40m;;{i} Redirected 301')
+		elif resp.status_code==404:
+			print(f'\033[1;31;40m{i} 404 Not Found')
+		elif resp.status_code==403:
+			print(f"\033[1;36;40m{i} 403 Forbidden/Redirecting")
+		elif resp.status_code==400:
+			print(f'\033[1;30;40m{i} 400 Bad Request')
+		else:
+			print(f"\033[1;34;40m{i} Status : {resp.status_code}")
+	except requests.ConnectionError:
+		print("\033[1;31;40m" + i + " 404 Not Found")
+	
+	
+for i in f:
+	thread = threading.Thread(target=full,args=(i,))
+	thread.start()
